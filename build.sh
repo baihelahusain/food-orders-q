dba0902533afb5c1993cccb88fce2d96328ce879
Baihela Hussain <baihelahusain@gmail.com>
2025-04-17 01:32:33 +0530

    Add superuser creation to build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate 
+python manage.py migrate
+
+# Create superuser
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('baihelahusain', 'baihelahusain@gmail.com', 'bah@7865354') if not User.objects.filter(username='baihelahusain').exists() else None" | python manage.py shell 
